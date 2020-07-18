b747811c8c43b9cd4b979826697a0f8dd3bcf200
Alex Chiu <alexchiu11@gmail.com>
2020-07-18 13:55:58 +0100

    ✨🙌✨

---
+++ functions/hello.ts
@@ -0,0 +1,32 @@
+/**
+ * Main entry point for journal endpoint.
+ */
+import { APIGatewayProxyEvent, APIGatewayProxyCallback } from "aws-lambda";
+
+export const handler = async function (
+  event: APIGatewayProxyEvent,
+  _context: any,
+  callback: APIGatewayProxyCallback
+) {
+  if (event.httpMethod === "GET") {
+    if (event.queryStringParameters?.name) {
+      callback(null, {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(`Hello ${event.queryStringParameters.name}`)
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify("Hello World")
+      });
+    }
+  } else {
+    callback(null, {
+      statusCode: 402,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify("Method not supported")
+    });
+  }
+};
